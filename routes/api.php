494a8dea9dde67c3f44b42c94ec08c161f5e96f8
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2017-12-04 12:56:44 +0700

    route

--- routes/api.php
+++ routes/api.php
@@ -13,21 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
-// Route::resource('pegawai', 'PegawaiController')->middleware(['auth:api']);
-// Route::resource('pelanggan', 'PelangganController')->middleware(['auth:api']);
-// Route::resource('supplier', 'SupplierController')->middleware(['auth:api']);//Supplier + Bahan Baku
-// Route::resource('produk', 'ProdukController')->middleware(['auth:api']);
-// Route::resource('bahan-baku', 'BahanBakuController')->middleware(['auth:api']);
-// Route::resource('pesanan', 'PesananController')->middleware(['auth:api']);
+Route::resource('pegawai', 'PegawaiController')->middleware(['auth:api']);
+Route::resource('pelanggan', 'PelangganController')->middleware(['auth:api']);
+Route::resource('supplier', 'SupplierController')->middleware(['auth:api']);//Supplier + Bahan Baku
+Route::resource('produk', 'ProdukController')->middleware(['auth:api']);
+Route::resource('bahan-baku', 'BahanBakuController')->middleware(['auth:api']);
+Route::resource('pesanan', 'PesananController')->middleware(['auth:api']);
 
-// Route::group(['prefix'=>'select', 'middleware'=> 'auth:api'], function(){
-// 	Route::get('/bahan-baku', function(){
-// 		$bahan = App\Entities\BahanBaku::all();
-// 		return response()->json($bahan);
-// 	});	
-// });
+Route::group(['prefix'=>'select', 'middleware'=> 'auth:api'], function(){
+	Route::get('/bahan-baku', function(){
+		$bahan = App\Entities\BahanBaku::all();
+		return response()->json($bahan);
+	});	
+});
--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,16 @@
 |
 */
 
-// Auth::routes();
+Auth::routes();
 
-// // Route::get('/', 'HomeController@index')->name('home');
-// Route::get('/', function(){
-// 	return view('home');
-// })->name('home')->middleware(['auth']);
+// Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', function(){
+	return view('home');
+})->name('home')->middleware(['auth']);
 
-// Route::get('pegawai', 'PegawaiController@index')->middleware(['auth'])->name('pegawai');
-// Route::get('pelanggan', 'PelangganController@index')->middleware(['auth'])->name('pelanggan');
-// Route::get('supplier', 'SupplierController@index')->middleware(['auth'])->name('supplier');
-// Route::get('produk', 'ProdukController@index')->middleware(['auth'])->name('produk');
-// Route::get('bahan-baku', 'BahanBakuController@index')->middleware(['auth'])->name('bahan-baku');
-// Route::get('pesanan', 'PesananController@index')->middleware(['auth'])->name('pesanan');
+Route::get('pegawai', 'PegawaiController@index')->middleware(['auth'])->name('pegawai');
+Route::get('pelanggan', 'PelangganController@index')->middleware(['auth'])->name('pelanggan');
+Route::get('supplier', 'SupplierController@index')->middleware(['auth'])->name('supplier');
+Route::get('produk', 'ProdukController@index')->middleware(['auth'])->name('produk');
+Route::get('bahan-baku', 'BahanBakuController@index')->middleware(['auth'])->name('bahan-baku');
+Route::get('pesanan', 'PesananController@index')->middleware(['auth'])->name('pesanan');
