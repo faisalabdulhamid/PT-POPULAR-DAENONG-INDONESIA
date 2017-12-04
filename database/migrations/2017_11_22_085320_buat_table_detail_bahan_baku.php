527369f3afc5cb2bcc917db5430fae019f42e60a
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2017-12-04 12:36:43 +0700

    migrate

---
+++ database/migrations/2017_11_22_085320_buat_table_detail_bahan_baku.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTableDetailBahanBaku extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_bahan_baku', function (Blueprint $table) {
+            // $table->increments('id');
+            // $table->timestamps();
+            $table->unsignedInteger('jumlah');
+            $table->unsignedInteger('supplier_id');
+            $table->unsignedInteger('bahan_baku_id');
+
+            $table->foreign('supplier_id')
+                ->references('id')
+                ->on('supplier')
+                ->onDelete('cascade');
+            $table->foreign('bahan_baku_id')
+                ->references('id')
+                ->on('bahan_baku')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_bahan_baku');
+    }
+}
