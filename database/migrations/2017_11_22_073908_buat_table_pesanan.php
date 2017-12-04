aa2e15609d7b5adab89a20e0bfe5f42de844a96e
Faisal Abdul Hamid <faisal.abdulhamid@gmail.com>
2017-12-04 12:45:07 +0700

    migrate

---
+++ database/migrations/2017_11_22_073908_buat_table_pesanan.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTablePesanan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanan', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('pelanggan_id');
+            $table->unsignedInteger('pegawai_id');
+            $table->date('tanggal');
+            $table->unsignedInteger('total_bayar');
+            $table->enum('status', ['lunas', 'belum lunas']);
+            $table->timestamps();
+
+
+            $table->foreign('pelanggan_id')
+                ->references('id')
+                ->on('pelanggan')
+                ->onDelete('cascade');
+            $table->foreign('pegawai_id')
+                ->references('id')
+                ->on('pegawai')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanan');
+    }
+}
